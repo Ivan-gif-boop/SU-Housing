7a8b44fdf43d1ef11051f29f5ea379fe388722e6
Ivan-gif-boop <Ivan.wachira@strathmore.edu>
2026-06-30 12:13:59 +0300

    Fixed logout.php and deleted duplicate index.php

--- logout.php
+++ logout.php
@@ -1,14 +1,9 @@
 <?php
 // logout.php
-// ─────────────────────────────────────────
-// BACKEND — Michelle fills this in
-// ─────────────────────────────────────────
-// session_start();
-// session_unset();
-// session_destroy();
-// ─────────────────────────────────────────
+session_start();
+session_unset();
+session_destroy();
 
-// Redirect to login for now
 header('Location: /SU-Housing/login.php');
 exit;
 ?>
